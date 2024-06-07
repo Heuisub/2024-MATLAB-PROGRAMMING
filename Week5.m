01bdf9996ecd4d79a06f881c5e7eedd729ca1e51
Heuisub <81272875+Heuisub@users.noreply.github.com>
2024-06-07 13:34:07 +0900

    Create Week5.m

---
+++ Week5.m
@@ -0,0 +1,12 @@
+a = input()
+
+disp(variable_name) or disp('text as string')
+fprintf(text typed in as string)
+
+
+fprintf(' %8.2f', a)
+"%number.number character"
+  ------ ----- 
+  space  back number count
+  
+  % \n \b \t 등등...
